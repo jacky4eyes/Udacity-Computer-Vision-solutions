5fb1a855a4e63941f8bd0400a4e2f91363a345d6
Jacky Han <jacky4eyes@gmail.com>
2020-10-24 15:03:17 +1100

    update utilities

--- utilities/gaussian_gradient.m
+++ utilities/gaussian_gradient.m
@@ -1,6 +1,6 @@
 %% return x-derivative and y-derivative images
-% [img_x,img_y] = gaussian_gradient(img,g_size ,sigma)
-function [img_x,img_y] = gaussian_gradient(img,g_size ,sigma)
+% [img_x, img_y] = gaussian_gradient(img,g_size ,sigma)
+function [img_x, img_y] = gaussian_gradient(img, g_size, sigma)
     x = (-floor(g_size/2)):floor(g_size/2);
     H = fspecial('gaussian',g_size,sigma);
     H_x = H.*(repmat(-x./(sigma^2),g_size,1)); % x gaussian derivative kernel
