8bf7c09763feb334badd4b5c4eb3fd0b257190ee
Jacky Han <jacky4eyes@gmail.com>
2020-10-24 15:17:37 +1100

    update utilities

--- utilities/make_M_Matrix.m
+++ utilities/make_M_Matrix.m
@@ -4,7 +4,7 @@
 % W is the window (kernel with weights)
 function M = make_M_Matrix(I_x, I_y, W)
     
-    % get second-order derivatives
+    % get products of first-order derivatives
     I_xx = I_x.^2;
     I_yy = I_y.^2;
     I_xy = I_x.*I_y;
