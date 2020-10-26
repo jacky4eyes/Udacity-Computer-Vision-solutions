f1aaac874aa729d3dc6999e211898fff7089437f
Jacky Han <jacky4eyes@gmail.com>
2020-10-26 22:26:57 +1100

    fix gaussian_gradient.m reverse sign bug

--- utilities/gaussian_gradient.m
+++ utilities/gaussian_gradient.m
@@ -3,8 +3,8 @@
 function [img_x, img_y] = gaussian_gradient(img, g_size, sigma)
     x = (-floor(g_size/2)):floor(g_size/2);
     H = fspecial('gaussian',g_size,sigma);
-    H_x = H.*(repmat(-x./(sigma^2),g_size,1)); % x gaussian derivative kernel
-    H_y = H.*(repmat(-x'./(sigma^2),1,g_size)); % y gaussian derivative kernel
+    H_x = H.*(repmat(x./(sigma^2),g_size,1)); % x gaussian derivative kernel
+    H_y = H.*(repmat(x'./(sigma^2),1,g_size)); % y gaussian derivative kernel
     img_x = imfilter(img,H_x);
 %     img_x = conv2(img,H_x);
     img_y = imfilter(img,H_y);
