eae7239cfacb93f40790c9736c202ecb49430fee
Jacky Han <jacky4eyes@gmail.com>
2020-11-21 15:33:41 +1100

    begin PS7

---
+++ ps7_matlab/ps7.m
@@ -0,0 +1,10 @@
+%% ps7
+clear all; clc;
+
+% first, make sure your current directory is where this script is
+% then add to path all functions essential to this project
+addpath(genpath('../utilities'));  
+
+
+%%
+
